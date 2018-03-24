4157f6db3c088c650db5ce1504ce456dc22ac73a
CEPAG\k.verma <k.verma@kentaur-solutions.com>
2018-03-24 15:52:23 +0100

    # commit test merge and commit

--- spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/FeignAutoConfiguration.java
+++ spring-cloud-netflix-core/src/main/java/org/springframework/cloud/netflix/feign/FeignAutoConfiguration.java
@@ -58,7 +58,9 @@ import javax.annotation.PreDestroy;
 @ConditionalOnClass(Feign.class)
 @EnableConfigurationProperties({FeignClientProperties.class, FeignHttpClientProperties.class})
 public class FeignAutoConfiguration {
-
+	
+	//verma chnages
+	private String verma;
 	@Autowired(required = false)
 	private List<FeignClientSpecification> configurations = new ArrayList<>();
 
